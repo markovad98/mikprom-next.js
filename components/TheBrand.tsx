160303054e227127c59f91714afc4f6b2552bec3
Danil <daniklucky38@gmail.com>
2020-04-21 17:41:39 +0400

    style change catalog

--- components/TheBrand.tsx
+++ components/TheBrand.tsx
@@ -4,8 +4,8 @@ const TheBrand = () => {
     return (
         <section className="brand container">
             <h2 className="choice__brand">ВЫБЕРЕТЕ БРЕНД:</h2>
-            <a ><img className="logo-chick" src={require("../public/images/logos/chick.svg")} alt="chick"/></a>
-            <a ><img className="logo-micprom" src={require("../public/images/logos/mikpromLogo.svg")} alt="micprom"/></a>
+            <a ><img className="logo-chick" src={require("../public/images/logos/chick.png")} alt="chick"/></a>
+            <a ><img className="logo-micprom" src={require("../public/images/logos/mikpromLogo.png")} alt="micprom"/></a>
         </section>
         )
     }
