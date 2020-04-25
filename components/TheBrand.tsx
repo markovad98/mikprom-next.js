e327009c29651838486d5991553ef7c521cd0dd4
Danil <daniklucky38@gmail.com>
2020-04-25 18:55:00 +0400

    change TheBrand

--- components/TheBrand.tsx
+++ components/TheBrand.tsx
@@ -4,8 +4,10 @@ const TheBrand = () => {
     return (
         <section className="brand container">
             <h2 className="choice__brand">ВЫБЕРЕТЕ БРЕНД:</h2>
-            <a ><img className="logo-chick" src={require("../public/images/logos/chick.png")} alt="chick"/></a>
-            <a ><img className="logo-micprom" src={require("../public/images/logos/mikpromLogo.png")} alt="micprom"/></a>
+            <div className="logo-brand">
+                <a ><img className="logo-chick" src={require("../public/images/logos/chick.png")} alt="chick"/></a>
+                <a ><img className="logo-micprom" src={require("../public/images/logos/mikpromLogo.png")} alt="micprom"/></a>
+            </div>
         </section>
         )
     }
