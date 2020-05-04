e0f796edf01e4a6c5322045c911d44bc7a6f37fa
Алексей Марков <alexey.markov@opendev.com>
2020-05-04 22:57:51 +0400

    fix fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ProductsProvider } from "../context/ProductsContext";
 import { NewsProvider } from "../context/NewsContext";
 import Layout from "../layouts";
 import { CartProvider } from "../context/CartContext";
+import Head from "next/head";
 
 type TProps = {
   Component: React.FC;
@@ -13,6 +14,14 @@ type TProps = {
 const MyApp: React.FC<TProps> = ({ Component, pageProps }) => {
   return (
     <div className="app">
+      <Head>
+        <title>Микпром</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <NewsProvider>
         <ProductsProvider>
           <CartProvider>
